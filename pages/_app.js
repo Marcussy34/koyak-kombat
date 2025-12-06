7063f4187283de0d547d3c47c213f2ebd1df29df
“Marcus” <115784464+Marcussy34@users.noreply.github.com>
2025-12-06 12:07:09 +0800

    added BackgroundMusic component to provide lobby music throughout the application

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import BackgroundMusic from "@/components/BackgroundMusic";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <BackgroundMusic />
+    </>
+  );
 }
